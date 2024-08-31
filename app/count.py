a6d478bcb452015ccd50b2004d7554a3fd352eb0
B Tasker <github@bentasker.co.uk>
2024-08-31 14:35:12 +0100

    feat: calculate rate of cycling

--- app/count.py
+++ app/count.py
@@ -5,21 +5,58 @@
 #
 
 
-import RPi.GPIO as GPIO
 import datetime
+import os
+import RPi.GPIO as GPIO
+import sys
+import time
  
-GPIO_NUM=4
+GPIO_NUM = int(os.getenv("RPI_GPIO_NUM", 4))
+
+# This is based on what the computer does. I could probably
+# grumble for a while about how it doesn't take the level of effort
+# at different resistances into account though
+CYCLES_PER_CALORIE=int(os.getenv("CYCLES_PER_CALORIE", 7 ))
+ 
+# How often should we check counts
+POLL_INTERVAL=int(os.getenv("CYCLES_POLL_INTERVAL", 5 ))
+
  
 def detected_change(channel):
+    ''' A change to the GPIO pin was detected
+    '''
+    global COUNTER
     if GPIO.input(channel) == GPIO.HIGH:
         print('\n1  at ' + str(datetime.datetime.now()))
+        COUNTER += 1
+
+
+# Define the counter
+COUNTER = 0
+LAST_COUNTER = 0
+
+if POLL_INTERVAL < 1:
+    print("Poll interval must be at least 1")
+    sys.exit(1)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_NUM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(GPIO_NUM, GPIO.RISING, callback=detected_change)
  
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIO_NUM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(GPIO_NUM, GPIO.BOTH, callback=detected_change)
- 
-    message = input('Press enter to exit')
- 
-finally:
-    GPIO.cleanup()
+while True:
+    time.sleep(POLL_INTERVAL)
+    difference = COUNTER - LAST_COUNTER
+    LAST_COUNTER = COUNTER
+    
+    # What's the average rate of rotation?
+    rate = difference / POLL_INTERVAL
+
+    # TODO: calorie calculation
+    #
+    # This one's a bit harder - do we calculate fractions
+    # or do we just carry any difference over?
+    
+    print(f"{difference} cycles (avg {rate}/s)")
+    
+
+GPIO.cleanup()
