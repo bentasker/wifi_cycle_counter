bbfaa67bce98d4369cc155b51ebd49017b3b96dd
B Tasker <github@bentasker.co.uk>
2024-08-31 14:44:01 +0100

    feat: only emit stats periodically (misc/wifi-cycle-counter#1)

--- app/count.py
+++ app/count.py
@@ -21,14 +21,34 @@ CYCLES_PER_CALORIE=int(os.getenv("CYCLES_PER_CALORIE", 7 ))
 # How often should we check counts
 POLL_INTERVAL=int(os.getenv("CYCLES_POLL_INTERVAL", 5 ))
 
+# How often should we write out stats?
+WRITE_INTERVAL=int(os.getenv("CYCLES_WRITE_INTERVAL", 30 ))
+WRITE_NO_CHANGE= (os.getenv("CYCLES_WRITE_NOCHANGE", "true" ).lower()  == "true")
+
  
 def detected_change(channel):
     ''' A change to the GPIO pin was detected
     '''
     global COUNTER
     if GPIO.input(channel) == GPIO.HIGH:
-        print('\n1  at ' + str(datetime.datetime.now()))
+        #print('\n1  at ' + str(datetime.datetime.now()))
         COUNTER += 1
+
+def aggregate_and_write(buffer):
+    ''' Take a stats buffer and generate a datapoint from it
+    '''
+    
+    first_time = buffer[0][0]
+    end_time = buffer[-1][0]
+    time_period = end_time - first_time
+    
+    total_cycles = 0
+    for entry in buffer:
+        total_cycles += entry[1]
+    
+    rate = total_cycles / time_period
+    
+    print(f"{total_cycles} cycles (avg {rate}/s) measured over {time_period}s")    
 
 
 # Define the counter
@@ -42,21 +62,32 @@ if POLL_INTERVAL < 1:
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_NUM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(GPIO_NUM, GPIO.RISING, callback=detected_change)
- 
+
+stats_buffer = []
+last_write = time.time()
 while True:
     time.sleep(POLL_INTERVAL)
     difference = COUNTER - LAST_COUNTER
     LAST_COUNTER = COUNTER
+    now = time.time()
     
-    # What's the average rate of rotation?
-    rate = difference / POLL_INTERVAL
+    # rate = difference / POLL_INTERVAL
 
     # TODO: calorie calculation
     #
     # This one's a bit harder - do we calculate fractions
     # or do we just carry any difference over?
-    
-    print(f"{difference} cycles (avg {rate}/s)")
+
+    stats_buffer.append([
+        now,
+        difference
+        ])
+
+    # Have we reached a write interval?
+    if (now - last_write) > WRITE_INTERVAL:
+        aggregate_and_write(stats_buffer)
+        stats_buffer = []
+        last_write = now
     
 
 GPIO.cleanup()
